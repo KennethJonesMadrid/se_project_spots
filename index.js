717906b9cfcc3c153f572ede60ea6875122fa11f
Kenneth Jones <alexanderjonesmadrid@gmail.com>
2024-09-18 13:54:06 -0400

    update to project stage 1

---
+++ index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "A man standing on the beach next to palm trees",
+    link: "https://unsplash.com/photos/a-man-standing-on-a-beach-next-to-palm-trees-aGiM4CYG27s",
+  },
+  {
+    name: "Waking up in the Venezuelan coast",
+    link: "https://unsplash.com/photos/person-walking-on-beach-during-daytime-fP44mZBa2zQ",
+  },
+  {
+    name: "White clouds and blue skies during daytime in Rio",
+    link: "https://unsplash.com/photos/white-clouds-and-blue-sky-during-daytime-XAiPD7cY7aQ",
+  },
+  {
+    name: "A formidable beast",
+    link: "https://unsplash.com/photos/closeup-photo-of-brown-tabby-cat-AM9ZtoUss68",
+  },
+  {
+    name: "Unidentified aerial phenomena on a green sky",
+    link: "https://unsplash.com/photos/green-and-black-trees-under-blue-sky-HWQXIYbs8PM",
+  },
+  {
+    name: "Freedom achieved",
+    link: "https://unsplash.com/photos/two-person-running-on-seaside-beach-during-daytime-Fg9l2rojs24",
+  },
+];
+
+console.log(initialCards);
