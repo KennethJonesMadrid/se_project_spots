73b310363551023d1fcc50cb87535692ba729955
Kenneth Jones <alexanderjonesmadrid@gmail.com>
2024-09-30 15:22:12 -0400

    new commit for stage 3

--- index.js
+++ index.js
@@ -25,4 +25,24 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editModal = document.querySelector("#edit-profile-modal");
+
+const closeModalButton = document.querySelector(".modal__close-button");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", function () {
+  openModal();
+});
+
+closeModalButton.addEventListener("click", function () {
+  closeModal();
+});
