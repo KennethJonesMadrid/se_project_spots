cc3f3dc58d9dce00b64d6594c87cae8a01910892
Kenneth Jones <alexanderjonesmadrid@gmail.com>
2024-10-07 13:09:39 -0400

    new commit for final stage of project 4

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "A man standing on the beach next to palm trees",
-    link: "https://unsplash.com/photos/a-man-standing-on-a-beach-next-to-palm-trees-aGiM4CYG27s",
+    link: "https://images.unsplash.com/photo-1722532356878-262a1f877958?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Waking up in the Venezuelan coast",
-    link: "https://unsplash.com/photos/person-walking-on-beach-during-daytime-fP44mZBa2zQ",
+    link: "https://images.unsplash.com/photo-1581260502159-c9204828728c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "White clouds and blue skies during daytime in Rio",
-    link: "https://unsplash.com/photos/white-clouds-and-blue-sky-during-daytime-XAiPD7cY7aQ",
+    link: "https://images.unsplash.com/photo-1583978618388-c9726a050ac1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "A formidable beast",
-    link: "https://unsplash.com/photos/closeup-photo-of-brown-tabby-cat-AM9ZtoUss68",
+    link: "https://images.unsplash.com/photo-1516853996367-a62f75433187?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Unidentified aerial phenomena on a green sky",
-    link: "https://unsplash.com/photos/green-and-black-trees-under-blue-sky-HWQXIYbs8PM",
+    link: "https://images.unsplash.com/photo-1612512836264-5e58fab88bf0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Freedom achieved",
-    link: "https://unsplash.com/photos/two-person-running-on-seaside-beach-during-daytime-Fg9l2rojs24",
+    link: "https://images.unsplash.com/photo-1470093879860-2221625b28be?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
@@ -33,6 +33,8 @@ const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-profile-modal");
 
+const editFormElement = editModal.querySelector(".modal__form");
+
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 
 const editModalDescriptionInput = editModal.querySelector(
@@ -40,6 +42,27 @@ const editModalDescriptionInput = editModal.querySelector(
 );
 
 const closeModalButton = document.querySelector(".modal__close-button");
+
+const cardTemplate = document.querySelector("#card-template");
+
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameElement = cardElement.querySelector(".card__title");
+  cardNameElement.textContent = data.name;
+
+  const cardImageElement = cardElement.querySelector(".card__image");
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
+  // select the image element
+
+  return cardElement;
+  //assign values to the image src and alt
+}
 
 function openModal() {
   editModal.classList.add("modal_opened");
@@ -51,6 +74,13 @@ function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+  closeModal();
+}
+
 profileEditButton.addEventListener("click", function () {
   openModal();
 });
@@ -58,3 +88,10 @@ profileEditButton.addEventListener("click", function () {
 closeModalButton.addEventListener("click", function () {
   closeModal();
 });
+
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
